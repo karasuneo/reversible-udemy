4e680dd19fe96f68d5faadf6f79c9250dff99c41
Yusei Mizutani <yusei4520030401@gmail.com>
2023-07-12 08:58:58 +0900

    非同期

--- training/asynchronous.ts
+++ training/asynchronous.ts
@@ -1,1 +1,29 @@
 import fs from "fs";
+import util from "util";
+
+const promisifyReadFile = util.promisify(fs.readFile);
+
+async function main() {
+  const data = await promisifyReadFile("../package.json");
+  const fileContent = data.toString();
+  console.log(fileContent);
+
+  //   const readFilePromise = promisifyReadFile("../package.json");
+
+  //   readFilePromise.then((data) => {
+  //     const fileContent = data.toString();
+  //     console.log(fileContent);
+  //   });
+
+  //   ここから
+  //   let fileContent: string = "Not loaded";
+
+  //   fs.readFile("../package.json", (err, data) => {
+  //     fileContent = data.toString();
+  //     console.log(fileContent);
+  //   });
+
+  //   console.log(fileContent);
+}
+
+main();
